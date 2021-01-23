b80190f35c334794ffb4f3aebfa62c537d062cd3
Manu Sabarrós <manusabarros@gmail.com>
2021-01-23 20:31:58 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+const Index = () => {
+    return (
+        <div className={styles.Index}>
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
+
+export default Index;
